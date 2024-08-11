34643ba2f3f59688c23434b88e9427b7eecd25eb
Reniel John Supan <xrenzkie21@gmail.com>
2024-08-11 14:52:08 +0800

    startUp panel Integrated

---
+++ app/src/main/java/com/example/pathfit3/startupPanel.java
@@ -0,0 +1,38 @@
+package com.example.pathfit3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class startupPanel extends AppCompatActivity {
+    private Button btnGetStarted;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_startup_panel);
+
+        initViews();
+        btnGetStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(startupPanel.this, homeModule.class);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void initViews(){
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+
+    }
+}
