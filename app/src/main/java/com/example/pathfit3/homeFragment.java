9543645f5d9186a5ee24daf58a580edaff6cc44d
Reniel John Supan <xrenzkie21@gmail.com>
2024-08-12 10:32:55 +0800

    Home Module design succesfully but no logic in the .java file

---
+++ app/src/main/java/com/example/pathfit3/homeFragment.java
@@ -0,0 +1,21 @@
+package com.example.pathfit3;
+
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class homeFragment extends Fragment {
+    CardView crdLesson,crdQuiz, crdTutorial, crdAbout;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
