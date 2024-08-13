279d4d54a90acabddb2f8f95ed736a8b32fe94ba
Reniel John Supan <xrenzkie21@gmail.com>
2024-08-13 14:29:20 +0800

    Finish creating lesson 1

--- app/src/main/java/com/example/pathfit3/aboutUsFragment.java
+++ app/src/main/java/com/example/pathfit3/aboutUsFragment.java
@@ -7,18 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
+public class aboutUsFragment extends Fragment {
 
-public class lessonsFragment extends Fragment {
-    ImageButton lesson1_image, lesson2_image;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_lessons, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_about_us, container, false);
     }
-
 }
